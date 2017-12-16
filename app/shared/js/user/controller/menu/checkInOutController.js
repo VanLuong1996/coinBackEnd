c67a62a1a58281e818d4190b60623c57994d634e
ADMIN <hunglv2@gemvietnam.com>
2017-12-17 04:49:49 +0700

    server checkin out error ,

--- app/shared/js/user/controller/menu/checkInOutController.js
+++ app/shared/js/user/controller/menu/checkInOutController.js
@@ -1,12 +1,14 @@
 angular.module('user').controller('checkInOutController',
-    function ($scope, $rootScope, $state, checkInCheckOutRemoteServices) {
-        //      $scope.checkInOut = function () {
-        //          checkInCheckOutRemoteServices.checkInOut(function (data) {
-        //         alert("The action is processing");
-        //     }, function (data) {
-        //         alert(data.error);
-        //     });
-        // },
+    function userListController($scope, $rootScope, $state, userRemoteService,checkInCheckOutRemoteServices) {
+
+
+        $scope.checkInOut = function () {
+            userRemoteService.checkInOut(function (data) {
+                alert("The action is processing");
+            }, function (data) {
+                alert(data.error);
+            });
+        },
                  checkInCheckOutRemoteServices.getListCheckInCheckOut(function (data) {
                 $scope.checkInOutList = data;
                 // console.log($scope.transactionList);
