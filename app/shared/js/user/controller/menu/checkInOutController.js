dfa90e2d370561b7299832715ba8fac3ab57aaee
ADMIN <hunglv2@gemvietnam.com>
2017-12-17 07:46:01 +0700

    fixing check services html css

--- app/shared/js/user/controller/menu/checkInOutController.js
+++ app/shared/js/user/controller/menu/checkInOutController.js
@@ -1,7 +1,6 @@
 angular.module('user').controller('checkInOutController',
     function userListController($scope, $rootScope, $state, userRemoteService,checkInCheckOutRemoteServices) {
 
-
         $scope.checkInOut = function () {
             userRemoteService.checkInOut(function (data) {
                 alert("The action is processing");
